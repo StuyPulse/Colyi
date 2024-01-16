ccccc10e152836adc11777f49e8cb173caf82e38
Ian Shi <Ianshi1026@gmail.com>
2024-01-16 17:26:12 -0500

    Modify elevator to fit excelsior's climber
    Co-authored-by: jopy-man <jopy-man@users.noreply.github.com>
    Co-authored-by: Zixi52 <Zixi52@users.noreply.github.com>
    Co-authored-by: Lapcas <Lapcas@users.noreply.github.com>
    Co-authored-by: chillmjs <chillmjs@users.noreply.github.com>
    Co-authored-by: Ardian Agoes <ardianagoes@users.noreply.github.com>
    Co-authored-by: ambers7 <ambers7@users.noreply.github.com>
    Co-authored-by: Reya Miller
    <reyamiller@users.noreply.github.com>

--- src/main/java/com/stuypulse/robot/commands/elevator/ElevatorToTop.java
+++ src/main/java/com/stuypulse/robot/commands/elevator/ElevatorToTop.java
@@ -1,8 +1,9 @@
-package com.stuypulse.robot.commands;
+package com.stuypulse.robot.commands.elevator;
 
 import static com.stuypulse.robot.constants.Settings.Elevator.*;
 
 public class ElevatorToTop extends ElevatorToHeight {
+
     public ElevatorToTop() {
         super(MAX_HEIGHT);
     }
